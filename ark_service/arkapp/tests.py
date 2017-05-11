060fd1dbfe9ee47f63818ce1b00aaec6370065ab
Yaqi1023 <yulisazhang@yulisas-MacBook-Pro.local>
2017-05-11 15:53:19 -0700

    test

--- ark_service/arkapp/tests.py
+++ ark_service/arkapp/tests.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 from .models import Minter
 from .models import Ark
-from django.conf import settings
 
 # Create your tests here.
 class MinterModelTest(TestCase):
-	
+    def test_ark_exists(self): 
+        minter = Minter(name='test_ark_exists', prefix='1234', template='eeddeeddk')
+        minter.save()
+        pass
+
+    def test_mint(self): 
+        minter = Minter(name='test_mint', prefix='1234', template='eeddeeddk')
+        minter.save()
+        pass
+
+class ArkTestCase(TestCase): 
+    def test_bind(self):
+        pass
 		
